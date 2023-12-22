4d3dd6a8e67b628aa7f15d9c2dc49de4e167d7f8
tanayakarmakar <karmakar.tanaya@gmail.com>
2023-12-22 18:58:41 +0530

    controller error handling

--- src/main/java/com/app/orderservice/controller/PurchaseOrderController.java
+++ src/main/java/com/app/orderservice/controller/PurchaseOrderController.java
@@ -5,7 +5,11 @@ import com.app.orderservice.models.dto.PurchaseOrderResponseDTO;
 import com.app.orderservice.service.OrderFulfillmentService;
 import com.app.orderservice.service.OrderQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -19,8 +23,13 @@ public class PurchaseOrderController {
     private OrderQueryService orderQueryService;
 
     @PostMapping
-    public Mono<PurchaseOrderResponseDTO> createOrder(@RequestBody Mono<PurchaseOrderRequestDTO> purchaseOrderRequestDTOMono) {
-        return orderFulfillmentService.processOrder(purchaseOrderRequestDTOMono);
+    public Mono<ResponseEntity<PurchaseOrderResponseDTO>> createOrder(@RequestBody Mono<PurchaseOrderRequestDTO> purchaseOrderRequestDTOMono) {
+        return orderFulfillmentService.processOrder(purchaseOrderRequestDTOMono)
+                .map(ResponseEntity::ok)
+                .onErrorReturn(WebClientResponseException.class,
+                        ResponseEntity.badRequest().build())
+                .onErrorReturn(WebClientRequestException.class,
+                        ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build());
     }
 
     @GetMapping("/user/{userId}")
